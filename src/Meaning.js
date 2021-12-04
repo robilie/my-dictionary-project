f2b1c555d287b38d56432755265fa036929c5998
robilie <obilie.roberta@gmail.com>
2021-12-03 17:39:06 -0800

    Displayed synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -8,10 +9,13 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p> {definition.definition}</p>
-            <br />
             <p>
+              {definition.definition}
+              <br />
+
               <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
